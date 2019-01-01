5a83a94ebaa4b89c0fd4be230519c4ead15d5d81
Ph055a <ph055a@protonmail.com>
2019-01-01 11:20:16 -0500

    minor changes

--- server-setup.sh
+++ server-setup.sh
@@ -1,23 +1,20 @@
 sudo apt update; sudo apt upgrade -y
 sudo apt install -y \
 ruby-dev \
-golang \
-python-pip \
-python3-pip \
-lua5.3
+asciinema
 
 sudo snap install micro --classic
 sudo snap install docker
 sudo snap install amass
 sudo snap install powershell-preview --classic
 
-sudo gem install easywins
-sudo gem install wikiranger
-sudo gem install shodanz
-sudo gem install ryo
-sudo gem install gauntlt
-sudo gem install bundler
-sudo gem install aquatone
+# sudo gem install easywins
+# sudo gem install wikiranger
+# sudo gem install shodanz
+# sudo gem install ryo
+# sudo gem install gauntlt
+# sudo gem install bundler
+# sudo gem install aquatone
 
 mkdir ~/Tools; cd ~/Tools
 
