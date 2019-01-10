28cb64d9f2e4d408f30e3d98b4e23607db458990
Ph055a <ph055a@protonmail.com>
2019-01-10 13:13:05 -0500

    rewrite

--- Tools/server-setup.sh
+++ Tools/server-setup.sh
@@ -1,4 +1,3 @@
-mkdir ~/.go
 sudo apt update; sudo apt upgrade -y
 
 sudo apt install -y \
@@ -20,7 +19,7 @@ nmap \
 jq \
 dnswalk \
 dnstracer \
-dmitry 
+dmitry
 
 sudo gem update --system
 sudo pip install --upgrade pip
@@ -44,9 +43,10 @@ $g watir
 $g bundler
 $g aquatone
 
-mkdir ~/keys
-mkdir ~/Tools
-t="cd ~/Tools"
+mkdir ~/.go
+
+h="cd $HOME"
+c="git clone"
 
 # shodan & censys
 sudo -H pip install shodan
@@ -56,67 +56,65 @@ sudo -H pip install censys
 sudo -H pip3 install raccoon-scanner
 
 # Namechk
-$t; git clone https://github.com/HA71/Namechk.git
- 
+$h; $c https://github.com/HA71/Namechk.git
+
 # Twint
 sudo -H pip3 install twint
 
 # Zen
-$t; git clone https://github.com/s0md3v/Zen.git
+$h; $c https://github.com/s0md3v/Zen.git
 
 # Username Anarchy
-$t; git clone https://github.com/urbanadventurer/username-anarchy.git
+$h; $c https://github.com/urbanadventurer/username-anarchy.git
 
 # Spiderfoot
-$t
-wget --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0' 'https://www.spiderfoot.net/files/spiderfoot-2.12.0-src.tar.gz'
+cd ~/Tools; wget --user-agent 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:64.0) Gecko/20100101 Firefox/64.0' 'https://www.spiderfoot.net/files/spiderfoot-2.12.0-src.tar.gz'
 tar -xvzf spiderfoot-2.12.0-src.tar.gz
 sudo rm -rf spiderfoot-2.12.0-src.tar.gz
 
 # Zaproxy
-$t; wget https://github.com/zaproxy/zaproxy/releases/download/2.7.0/ZAP_2_7_0_unix.sh
+$h; wget https://github.com/zaproxy/zaproxy/releases/download/2.7.0/ZAP_2_7_0_unix.sh
 sudo rm -rf ZAP_2_7_0_unix.sh
 
 # Photon
-$t; git clone https://github.com/s0md3v/Photon.git; cd Photon
+$h; $c https://github.com/s0md3v/Photon.git; cd Photon
 sudo -H pip3 install -r requirements.txt
 
 # Pagodo
-$t; git clone https://github.com/opsdisk/pagodo.git
-cd pagodo
-sudo -H pip install -r requirements.txt; 
+$h; $c https://github.com/opsdisk/pagodo.git; cd pagodo
+sudo -H pip install -r requirements.txt;
 
 # Pymeta
-$t; git clone https://github.com/m8r0wn/pymeta.git; cd pymeta
+$h; $c https://github.com/m8r0wn/pymeta.git; cd pymeta
 sudo chmod +x setup.sh
-sudo ./setup.sh; 
+sudo ./setup.sh;
 
 # linkscrape
-$t; git clone https://github.com/NickSanzotta/linkScrape.git; cd linkScrape
-sudo -H pip install -r requirements.txt; 
+$h; $c https://github.com/NickSanzotta/linkScrape.git; cd linkScrape
+sudo -H pip install -r requirements.txt;
 
 # Domain_analyzer
-$t; git clone https://github.com/eldraco/domain_analyzer.git
+$h; $c https://github.com/eldraco/domain_analyzer.git
 
 # Skiptracer
-$t; git clone https://github.com/xillwillx/skiptracer.git; cd skiptracer
-sudo -H pip install -r requirements.txt; 
+$h; $c https://github.com/xillwillx/skiptracer.git; cd skiptracer
+sudo -H pip install -r requirements.txt;
 
 # SSRFmap
-$t; git clone https://github.com/dreadlocked/SSRFmap.git;cd SSRFmap
+$h; $c https://github.com/dreadlocked/SSRFmap.git; cd SSRFmap
 bundle install
 
 # datasploit
-$t; git clone https://github.com/DataSploit/datasploit.git; cd datasploit
+$h $c https://github.com/DataSploit/datasploit.git; cd datasploit
 sudo -H pip install -r requirements.txt
 
 # gasmask
-$t; git clone https://github.com/twelvesec/gasmask.git; cd gasmask
+$h; $c https://github.com/twelvesec/gasmask.git; cd gasmask
 sudo -H pip install -r requirements.txt
 
 # infoga
-$t; git clone https://github.com/m4ll0k/Infoga.git infoga; cd infoga
-python setup.py install; 
+$h; $c https://github.com/m4ll0k/Infoga.git infoga; cd infoga
+python setup.py install;
 
 # gitrob
 go get github.com/michenriksen/gitrob
@@ -127,9 +125,9 @@ cd $GOPATH/src/github.com/evilsocket/xray
 make; cd build; mv xray /usr/bin
 
 # phishing catcher
-$t; git clone https://github.com/x0rz/phishing_catcher.git; cd phishing_catcher
+$h; $c https://github.com/x0rz/phishing_catcher.git; cd phishing_catcher
 sudo -H pip install -r requirements.txt
 
 # Striker
-$t; git clone https://github.com/UltimateHackers/Striker; cd Striker
+$h; $c https://github.com/UltimateHackers/Striker; cd Striker
 sudo -H pip install -r requirements.txt
